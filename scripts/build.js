54ae189cdbf430166ee76aec4613633009537242
Ferdinand Prantl <prantlf@gmail.com>
2016-08-24 14:52:27 +0200

    Use the ncp npm module to copy lessc.js to the root directory

--- scripts/build.js
+++ scripts/build.js
@@ -1,22 +1,13 @@
-var fs = require('fs');
+var ncp = require('ncp').ncp,
+    source = 'node_modules/less/dist/less.js',
+    target = 'lessc.js';
 
-function copyFile(source, target) {
-  console.log('Copying ' + source + ' to ' + target + '...');
-  return new Promise(function(resolve, reject) {
-    var input = fs.createReadStream(source), output;
-    input.on('error', reject);
-    output = fs.createWriteStream(target);
-    output.on('error', reject);
-    output.on('finish', resolve);
-    input.pipe(output);
-  });
-}
-
-copyFile('node_modules/less/dist/less.js', 'lessc.js')
-  .then(function () {
-    process.exit(0);
-  })
-  .catch(function (error) {
+console.log('Copying ' + source + ' to ' + target + '...');
+ncp(source, target, function (error) {
+  if (error) {
     console.error(error.message);
     process.exit(1);
-  });
+  } else {
+    process.exit(0);
+  }
+});
