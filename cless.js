19da5c22274b1cc81c6518daab0849d583c33eb9
Tobi Schäfer <interface@p3k.org>
2018-06-20 10:57:37 +0200

    fix: plugin builder and indentation

--- cless.js
+++ cless.js
@@ -1,23 +1,23 @@
 define(function(require) {
-	var less = require('less');
+  var less = require('less');
 
-	var lessAPI = {
-		pluginBuilder: './wl-requirejs-less-builder'
-	};
+  var lessAPI = {
+    pluginBuilder: './cless-builder'
+  };
 
-	if (typeof window === 'undefined') {
-		lessAPI.load = function(n, r, load) { load(); };
-		return lessAPI;
-	}
+  if (typeof window === 'undefined') {
+    lessAPI.load = function(n, r, load) { load(); };
+    return lessAPI;
+  }
 
-	lessAPI.load = function(lessId, req, load, config) {
-		if (!config) config = {};
-		if (!config.less) config.less = {};
-		var savedState = config.less.component;
-		if (!savedState) config.less.component = true;
-		less.load.call(this, lessId, req, load, config);
-		if (config.less.component !== savedState) config.less.component = savedState;
-	};
+  lessAPI.load = function(lessId, req, load, config) {
+    if (!config) config = {};
+    if (!config.less) config.less = {};
+    var savedState = config.less.component;
+    if (!savedState) config.less.component = true;
+    less.load.call(this, lessId, req, load, config);
+    if (config.less.component !== savedState) config.less.component = savedState;
+  };
 
-	return lessAPI;
+  return lessAPI;
 });
